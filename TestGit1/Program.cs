9c932a8b1d05fd4f276df2114452cc1289b6e7ef
Tuan Tran <tuantran0416@gmail.com>
2025-07-10 21:47:21 +0700

    commit 2

--- TestGit1/Program.cs
+++ TestGit1/Program.cs
@@ -11,6 +11,7 @@ namespace TestGit1
         static void Main(string[] args)
         {
             Console.WriteLine("hello 1");
+            Console.WriteLine("hello 1");
             Console.ReadKey();
         }
     }
